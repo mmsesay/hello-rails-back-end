c028ad85cf92718a352ee40958aa1502dc32eeac
mmsesay <maej_01@hotmail.com>
2022-02-16 00:06:37 +0000

    Fix: nil exception error

--- app/controllers/v1/greeting_controller.rb
+++ app/controllers/v1/greeting_controller.rb
@@ -1,7 +1,7 @@
 class V1::GreetingController < ApplicationController
   def index
     @messages = Greeting.all
-    @random = @messages[rand(0..(@messages.count - 1))]
+    @random = @messages[rand(0..(@messages.count))]
 
     render json: { greeting: @random }.to_json
   end
